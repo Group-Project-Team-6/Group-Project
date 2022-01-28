8f933b9174d0be9a0d2ef5506a5e24d1e3eda201
hkjj293 <chrishuihuihui@gmail.com>
2022-01-28 01:19:14 +0000

    Added Class MapGenerator

--- Group Demo/Assets/Scripts/CullObstacle.cs
+++ Group Demo/Assets/Scripts/CullObstacle.cs
@@ -35,44 +35,5 @@ public class CullObstacle : MonoBehaviour
                 Shader.SetGlobalFloat("_IsCasted", 0.0f);
             }
         }
-        /*
-        while (hit.transform == null || hit.transform.gameObject != player)
-        {
-            if (Physics.Raycast(transform.position, dist.normalized, out hit, Mathf.Infinity, 1))
-            {
-                if (hit.transform.gameObject != player)
-                {
-                    Debug.Log(hit.transform.gameObject.name);
-                    Debug.Log(hit.transform.gameObject.layer);
-                    hideList.Add(hit.transform.gameObject);
-                    lastFrame.Remove(hit.transform.gameObject);
-                    Debug.Log(lastFrame.Count);
-                    Debug.Log(hideList.Count);
-                    hit.transform.gameObject.layer = 6;
-                }
-            }
-        }
-        
-        for (int i = 0; i < hideList.Count; i++)
-        {
-            Color c = hideList[i].GetComponent<Renderer>().material.color;
-            //Vector3 v = player.transform.position;
-            hideList[i].GetComponent<Renderer>().material.SetFloat("_IsCasted",1.0f);// = true;
-            hideList[i].layer = 0;
-            Debug.Log(hideList[i].layer);
-        }
-        for(int j = 0; j < lastFrame.Count; j++)
-        {
-            Color c = lastFrame[j].GetComponent<Renderer>().material.color;
-            //Vector3 v = player.transform.position;
-            lastFrame[j].GetComponent<Renderer>().material.SetFloat("_IsCasted",0.0f);
-
-            //lastFrame[j].GetComponent<Renderer>().forceRenderingOff = false;
-            lastFrame[j].layer = 0;
-        }
-        
-        lastFrame = new List<GameObject>(hideList);
-        hideList.Clear();
-        */
     }
 }

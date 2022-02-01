dcd027bceab2b552445837da55ed50f8658487be
David Wong <wongtszwing87@gmail.com>
2022-02-01 17:51:31 +0000

    Fixed linker issue

--- Group-Project/src/game/main.cpp
+++ Group-Project/src/game/main.cpp
@@ -5,5 +5,12 @@
 
 int main() {
 	std::cout << "Hello World" << std::endl;
+
+	int maxproxies = 1024;
+	btVector3 worldAabbMin = { -100, -100 , 100 };
+	btVector3 worldAabbMax = { -100, -100 , 100 };
+
+	btAxisSweep3* broadphase = new btAxisSweep3(worldAabbMin, worldAabbMax, maxproxies);
+
 	return 0;
 }

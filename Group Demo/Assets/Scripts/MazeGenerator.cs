b085ba723613b8ee0048027775a8350d9d2ea35d
b8036619 <b8036619@newcastle.ac.uk>
2022-02-01 11:40:27 +0000

    Maze Gen Code

--- Group Demo/Assets/Scripts/MazeGenerator.cs
+++ Group Demo/Assets/Scripts/MazeGenerator.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class MazeGenerator : MonoBehaviour
 {
     private string[,] level;
-    //private string[,] visited;
+
+    private int[,] potentialNodes;
+    int numberOfNodes;
+    private int[] usedNodes;
 
     private bool up = false;
     private bool down = false;
@@ -18,6 +21,8 @@ public class MazeGenerator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        numberOfNodes = 0;
+
         int x = 6;
         level = new string[x, x];
         //visited = new string[x * x,2];
@@ -25,7 +30,7 @@ public class MazeGenerator : MonoBehaviour
         int posX = 0;
         int posY = 0;
 
-        for (int i = 0; i < (x*x); i++)
+        for (int i = 0; i < (x * x); i++)
         {
             level[posX, posY] = "S";
             if (posX == 5)
@@ -35,7 +40,7 @@ public class MazeGenerator : MonoBehaviour
             }
             //if (posY == 5 && posX == 5)
             //{
-               // break;
+            // break;
             //}
 
         }
@@ -57,11 +62,19 @@ public class MazeGenerator : MonoBehaviour
             if (posX != 0 && level[posX - 1, posY] == "S") { left = true; }
             if (posX != 0 && level[posX + 1, posY] == "S") { right = true; }
 
-            if (up == false && down == false && left == false && right == false) { break; }
+            if (up == false && down == false && left == false && right == false) { break; } // add
 
+
+            if (up == true) { Assign(posX, posY - 1); }
+            if (down == true) { Assign(posX, posY + 1); }
+            if (left == true) { Assign(posX - 1, posY); }
+            if (right == true) { Assign(posX + 1, posY); }
+
+
+            /*
             r = Random.Range(0, 2);
             if (r != 1 && up == true) { level[posX, posY - 1] = "P"; }
-            else if (up == true) {  level[posX, posY - 1] = "#"; }
+            else if (up == true) { level[posX, posY - 1] = "#"; }
 
             r = Random.Range(0, 2);
             if (r != 1 && down == true) { level[posX, posY + 1] = "P"; }
@@ -74,10 +87,45 @@ public class MazeGenerator : MonoBehaviour
             r = Random.Range(0, 2);
             if (r != 1 && right == true) { level[posX + 1, posY - 1] = "P"; }
             else if (right == true) { level[posX + 1, posY - 1] = "#"; }
+            */
+
+            while (true)
+            {
+                int next = Random.Range(0, numberOfNodes - 1);
+                posX = potentialNodes[next, 0];
+                posY = potentialNodes[next, 1];
+
+                bool check = false;
+                for (int i = 0; i < numberOfNodes; i++)
+                {
+                    if (usedNodes[i] == next) { check = true; }
+                }
+                if (check == false) {
+
+                    usedNodes[usedNodes.Length + 1] = next;
+
+                    break;
+                }
+            }
+
+
+
 
         }
 
 
+    }
+
+    void Assign(int x, int y)
+    {
+        r = Random.Range(0, 2);
+        if (r != 1) {
+            level[x, y] = "P";
+            potentialNodes[numberOfNodes, 0] = x;
+            potentialNodes[numberOfNodes, 1] = y;
+            numberOfNodes++;
+        }
+        else { level[x, y] = "#"; }
     }
 
     // Update is called once per frame

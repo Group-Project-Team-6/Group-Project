c2ec2db50d24ec965c1d49e6b384a2505ce55a69
b1004949 <b1004949@newcastle.ac.uk>
2022-02-03 10:29:50 +0000

    Updated Cmake List

--- Group-Project/src/game/GameEntity.h
+++ Group-Project/src/game/GameEntity.h
@@ -1,4 +1,21 @@
 #pragma once
 
 #include "btBulletCollisionCommon.h"
+#include "btBulletDynamicsCommon.h"
+#include "../Plugins/OpenGLRendering/OGLRenderer.h"
+
+#include <vector>
+
+using std::vector;
+using std::string;
+
+class GameEntity {
+public:
+	GameEntity(string name = "");
+	~GameEntity();
+
+private:
+
+};
+
 

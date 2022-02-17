57bcb16afd0bfdbb4014f32852df6db933248e33
b8036619 <b8036619@newcastle.ac.uk>
2022-02-17 14:49:17 +0000

    Collectable Generator
    Created collectable generator

---
+++ Group Demo/Assets/Scripts/CollectableGenerator.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableGenerator : MonoBehaviour
+{
+    char[] mapChars;
+    char[,] map2DArray;
+    int[,] positions;
+    public GameObject collectable;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //read map
+        //find positions
+        //place collectables
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Collectables(int level, string map, int sizeX, int sizeY)
+    {
+        findPositions(map, sizeX, sizeY);
+        spawnCollectables(level);
+    }
+
+    void findPositions(string map, int sizeX, int sizeY)
+    {
+        map2DArray = new char[sizeX, sizeY];
+        mapChars = map.ToCharArray();
+        int i = 0;
+
+        for (int y = 0; sizeY > y; y++)
+        {
+            for (int x = 0; sizeX > x; x++)
+            {
+                map2DArray[x, y] = mapChars[i];
+                i++;
+            }
+        }
+
+        int numCollectables = ((sizeX * sizeY) / 50);
+        positions = new int[numCollectables, 2];
+        int placed = 0;
+
+        while (placed < numCollectables)
+        {
+            int y = Random.Range(0, sizeY);
+            int x = Random.Range(0, sizeX);
+
+            if (map2DArray[x, y] == 'S')
+            {
+                positions[placed, 0] = x;
+                positions[placed, 1] = y;
+                placed++;
+            }
+        }
+
+    }
+
+    void spawnCollectables(int level)
+    {
+        float translate = 0;
+
+        for (int i = 0; i < positions.GetLength(x); i++)
+        {
+            Instantiate(collectable, new Vector3(positions[i, 0] * translate, level * translate, positions[i, 1] * translate), Quaternion.identity);
+        }
+    }
+
+}

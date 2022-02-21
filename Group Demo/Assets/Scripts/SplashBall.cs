0e9eaecceb32b83de2ca202da5c871207695082c
hkjj293 <47801987+hkjj293@users.noreply.github.com>
2022-02-21 15:28:54 +0000

    lag when shoot

--- Group Demo/Assets/Scripts/SplashBall.cs
+++ Group Demo/Assets/Scripts/SplashBall.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class SplashBall : MonoBehaviour
 {
+    public float life = 1.0f;
     public float mass = 1.0f;
     public float radius = 1.0f;
     public Color32 color = Color.black;
     public Texture2D tex;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
 
     }
@@ -17,16 +18,62 @@ public class SplashBall : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        life -= Time.deltaTime;
     }
 
     void OnCollisionEnter(Collision collision)
     {
-        Destroy(gameObject);
+        Splash(collision);
     }
 
     void OnCollisionStay(Collision collision)
     {
+        Splash(collision);
+    }
+
+    private void Splash(Collision collision)
+    {
+        if (collision.gameObject.GetComponent<SplashBall>()) return;
+        //if (life > 0.0f)
+        //{
+        //    life *= 0.5f;
+        //    return;
+        //}
+        SplashOut(collision);
         Destroy(gameObject);
+    }
+
+    private void SplashOut(Collision collision)
+    {
+        if (radius > 0.25f)
+        {
+            Vector3 normal = collision.contacts[0].normal;
+            Vector3 pt = transform.position + collision.contacts[0].separation * normal.normalized;
+            Vector3 u = this.GetComponent<Rigidbody>().velocity;
+            for (int i = 0; i < 4; i++)
+            {
+                GameObject newSplash = Instantiate(
+                    gameObject,
+                    pt,
+                    new Quaternion(),
+                    this.transform.parent);
+                Vector3 dir = normal.normalized +
+                    new Vector3(
+                        Random.Range(-1.0f, 1.0f),
+                        Random.Range(-1.0f, 1.0f),
+                        Random.Range(-1.0f, 1.0f)
+                        );
+                
+                newSplash.GetComponent<Rigidbody>().velocity = 
+                    (this.GetComponent<Rigidbody>().velocity.normalized + dir).normalized
+                    * this.GetComponent<Rigidbody>().velocity.magnitude * 0.5f;
+                SplashBall subSplashBall = newSplash.GetComponent<SplashBall>();
+                subSplashBall.radius = radius * 0.5f;
+                subSplashBall.mass = mass * 0.5f;
+                subSplashBall.color = color;
+                subSplashBall.tex = tex;
+
+            }
+        }
     }
 }

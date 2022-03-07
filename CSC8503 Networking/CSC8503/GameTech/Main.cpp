cf22553500fb1561cb756d008475467b1bb1e56a
Srichand Dontha (PGT) <c1033252@newcastle.ac.uk>
2022-03-07 13:05:30 +0000

    update networking

---
+++ CSC8503 Networking/CSC8503/GameTech/Main.cpp
@@ -0,0 +1,120 @@
+#include "../../Common/Window.h"
+
+#include "../CSC8503Common/StateMachine.h"
+#include "../CSC8503Common/StateTransition.h"
+#include "../CSC8503Common/State.h"
+
+#include "../CSC8503Common/GameServer.h"
+#include "../CSC8503Common/GameClient.h"
+
+#include "../CSC8503Common/NavigationGrid.h"
+#include "../CSC8503Common/NavigationMesh.h"
+
+#include "TutorialGame.h"
+#include "NetworkedGame.h"
+
+using namespace NCL;
+using namespace CSC8503;
+
+#include <chrono>
+#include <thread>
+
+class TestPacketReceiver : public PacketReceiver {
+public:
+	TestPacketReceiver(string name) {
+		this->name = name;
+	}
+	void ReceivePacket(int type, GamePacket* payload, int source) {
+		if (type == String_Message) {
+			StringPacket* realPacket = (StringPacket*)payload;
+
+			string msg = realPacket->GetStringFromData();
+
+			std::cout << name << " received message: " << msg << std::endl;
+		}
+	}
+	string name;
+};
+
+void TestNetworking() {
+	NetworkBase::Initialise();
+
+	TestPacketReceiver serverReceiver("Server");
+	TestPacketReceiver clientReceiver("Client1");
+
+	TestPacketReceiver clientReceiver2("Client2");
+
+	int port = NetworkBase::GetDefaultPort();
+
+	GameServer* server = new GameServer(port, 2);
+	GameClient* client = new GameClient();
+	GameClient* client2 = new GameClient();
+
+	server->RegisterPacketHandler(String_Message, &serverReceiver);
+	client->RegisterPacketHandler(String_Message, &clientReceiver);
+	client2->RegisterPacketHandler(String_Message, &clientReceiver2);
+
+	bool canConnect = client->Connect(127, 0, 0, 1, port);
+	bool canConnect2 = client2->Connect(127, 0, 0, 1, port);
+
+	for (int i = 0; i < 100; ++i) {
+		server->SendGlobalPacket(StringPacket("Server says hello! " + std::to_string(i)));
+		client->SendPacket(StringPacket("Client1 says hello! " + std::to_string(i)));
+		client2->SendPacket(StringPacket("Client2 says hello! " + std::to_string(i)));
+		server->UpdateServer();
+		client->UpdateClient();
+		client2->UpdateClient();
+		std::this_thread::sleep_for(std::chrono::milliseconds(10));
+	}
+
+	NetworkBase::Destroy();
+}
+
+/*
+
+The main function should look pretty familar to you!
+We make a window, and then go into a while loop that repeatedly
+runs our 'game' until we press escape. Instead of making a 'renderer'
+and updating it, we instead make a whole game, and repeatedly update that,
+instead. 
+
+This time, we've added some extra functionality to the window class - we can
+hide or show the 
+
+*/
+int main() {
+	Window*w = Window::CreateGameWindow("CSC8503 Game technology!", 1280, 720);
+
+	if (!w->HasInitialised()) {
+		return -1;
+	}	
+	TestNetworking();
+
+	w->ShowOSPointer(false);
+	w->LockMouseToWindow(true);
+
+	TutorialGame* g = new TutorialGame();
+	w->GetTimer()->GetTimeDeltaSeconds(); //Clear the timer so we don't get a larget first dt!
+	while (w->UpdateWindow() && !Window::GetKeyboard()->KeyDown(KeyboardKeys::ESCAPE)) {
+		float dt = w->GetTimer()->GetTimeDeltaSeconds();
+		if (dt > 0.1f) {
+			std::cout << "Skipping large time delta" << std::endl;
+			continue; //must have hit a breakpoint or something to have a 1 second frame time!
+		}
+		if (Window::GetKeyboard()->KeyPressed(KeyboardKeys::PRIOR)) {
+			w->ShowConsole(true);
+		}
+		if (Window::GetKeyboard()->KeyPressed(KeyboardKeys::NEXT)) {
+			w->ShowConsole(false);
+		}
+
+		if (Window::GetKeyboard()->KeyPressed(KeyboardKeys::T)) {
+			w->SetWindowPosition(0, 0);
+		}
+
+		w->SetTitle("Gametech frame time:" + std::to_string(1000.0f * dt));
+
+		g->UpdateGame(dt);
+	}
+	Window::DestroyGameWindow();
+}
